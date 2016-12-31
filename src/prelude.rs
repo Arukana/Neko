97556ecf33f6edf11e6b8e71c124b950ed68a4d2
adjivas <adjivas@users.noreply.github.com>
2016-12-31 06:01:43 +0100

    extend prelude for pty and graphic

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,7 @@
-pub use super::{Neko, NekoError};
 pub use super::SPEC_ROOT;
+pub use super::{Neko, NekoError};
+pub use super::pty::{Shell, ShellError, ShellState};
 pub use super::dynamic::{Compositer, CompositerError};
 pub use super::dynamic::library::{Library, LibraryError};
+pub use super::editeur::{Graphic, GraphicError};
+

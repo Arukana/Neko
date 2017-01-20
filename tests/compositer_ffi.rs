3ba29d66aff985b0177db065721c6cca5db34e93
adjivas <adjivas@users.noreply.github.com>
2017-01-20 18:47:07 +0100

    ffi compositer more checks

--- tests/compositer_ffi.rs
+++ tests/compositer_ffi.rs
@@ -8,9 +8,21 @@ use neko::prelude::*;
 fn test_compositer_ffi() {
     {
         let mut compositer: Compositer = Compositer::new().unwrap();
+        assert_eq!(
+            compositer.get_state().get_persona().sheet,
+	        Sheet::None
+        );
+        assert_eq!(
+            &compositer.get_state().get_tooltip()[..6].iter().map(|c| c.get_glyph()).collect::<String>(),
+            &"\0\0\0\0\0\0".to_string()
+        );
 
         compositer.install(
             "https://github.com/Arukana/libnya.git"
+        );
+        assert_eq!(
+            compositer.get_state().get_persona().sheet,
+	        Sheet::Bust
         );
         assert_eq!(
             &compositer.get_state().get_tooltip()[..6].iter().map(|c| c.get_glyph()).collect::<String>(),

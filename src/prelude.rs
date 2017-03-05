9d5a026d034ddb941362118a12aab91b1bd9d938
adjivas <adjivas@users.noreply.github.com>
2017-03-05 22:34:03 +0100

    extend Control public for prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,7 +3,7 @@ pub use super::Parent;
 pub use super::PtyDisplay;
 pub use super::Display;
 pub use super::{Neko, NekoError};
-pub use super::pty::{Shell, ShellError, ShellState, Master, Winszed, Character, Key, Mouse};
+pub use super::pty::{Shell, ShellError, ShellState, Master, Winszed, Character, Control, Key, Mouse};
 pub use super::dynamic::{Compositer, CompositerError};
 pub use super::dynamic::library::{Library, LibraryError, LibraryState};
 pub use super::graphic::{Graphic, GraphicError, Sheet, Tuple, SPEC_MAX_DRAW, SPEC_MAX_XY};
